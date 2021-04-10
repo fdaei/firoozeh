c6ec902bc8ebb95ce66feab62095250f3b7bdb37
Mohammad Habibi <mdhi2000@gmail.com>
2021-04-10 11:35:41 +0430

    bug fixes

--- src/singup.js
+++ src/singup.js
@@ -1,58 +1,84 @@
-import React from 'react';
-import { Formik, Form } from 'formik';
-import { TextField } from './TextField';
-import * as yup from 'yup';
-import list from './list';
-function singup() {
-    const validate = yup.object().shape({
-        FirstName: yup.string().required('Required')
-        .max(15,'must be lessthan 16 characters') ,
-        LastName: yup.string().required('Required')
-        .max(20,'must be lessthan 20 characters') ,
-        email: yup.string().email('email is invalid')
-        .required(' email is Required'),    
-         PassWord:yup.string()
-        .max(6,'must be lessthan 16 characters') 
-        .required(' password is Required'),
-        ConfirmPasaword:yup.string()
-        .oneOf([yup.ref('password'),null],'password must match')
-        .required('Confirm password is requyired')
-      });
-    return (
-        <Formik
-            initialValues={{
-                FirstName: '',
-                LastName: '',
-                Email: '',
-                PassWord: '',
-                ConfirmPasaword: ''
-            }}
-            validationSchema={validate}
-            onSubmit={values=>{
-                const [list, setlist] = useState('')
-                values.map((index)=>(
-                    setlist(index)
-                ))
-            }
-            }
-         >
-            {formik => (
-                <div>
-                    <h1 className='my-4 font-weight-bold-display-4'> SignUp </h1>
-                    {console.log(formik.values)}
-                    <Form>
-                       <TextField label='FirstName' name='FirstName' type='text' />
-                       <TextField label='LastName' name='LastName' type='text' />
-                       <TextField label='Email' name='Email' type='email' />
-                       <TextField label='PassWord' name='PassWord' type='password' />
-                       <TextField label='ConfirmPasaword:' name='ConfirmPasaword' type='password' />
-                       <button className='btn btn-dark mt-3' type='submit'>Register</button>
-                       <button className='btn btn-danger mt-3 ml-3' type='reset'>Reset</button> 
-                    </Form>
-                </div>
-            )}
-        </Formik>
-    )
+import React, { useState } from "react"
+import { Formik, Form } from "formik"
+import { TextField } from "./TextField"
+import * as yup from "yup"
+// import list from './list';
+const fields = [
+  {
+    name: "FirstName",
+    type: "text",
+  },
+  {
+    name: "LastName",
+    type: "text",
+  },
+  {
+    name: "Email",
+    type: "email",
+  },
+  {
+    name: "PassWord",
+    type: "password",
+  },
+  {
+    name: "ConfirmPassword",
+    type: "password",
+  },
+]
+function SignUp() {
+  const [list, setList] = useState([])
+
+  const validate = yup.object().shape({
+    FirstName: yup.string().required("Required").max(15, "must be less than 16 characters"),
+    LastName: yup.string().required("Required").max(20, "must be less than 20 characters"),
+    Email: yup.string().email("email is invalid").required(" email is Required"),
+    PassWord: yup.string().max(6, "must be less than 16 characters").required(" password is Required"),
+    ConfirmPassword: yup
+      .string()
+      .oneOf([yup.ref("PassWord"), null], "password must match")
+      .required("Confirm password is requyired"),
+  })
+  return (
+    <Formik
+      initialValues={{
+        FirstName: "",
+        LastName: "",
+        Email: "",
+        PassWord: "",
+        ConfirmPassword: "",
+      }}
+      validationSchema={validate}
+      onSubmit={(values, actions) => {
+          console.log('ok')
+        setTimeout(() => {
+          Object.values(values).forEach(item => setList(prev => [...prev, item]))
+          console.log(values)
+          actions.setSubmitting(false)
+        }, 1000)
+      }}
+    >
+      {props => (
+        <Form>
+          <div>
+            <h1 className="my-4 font-weight-bold-display-4"> SignUp </h1>
+            {/* {console.log(formik.values)} */}
+            {fields.map((item, index) => (
+              <TextField key={index} label={item.name} name={item.name} type={item.type} />
+            ))}
+            <button className="btn btn-dark mt-3" type="submit">
+              Register
+            </button>
+            <button className="btn btn-danger mt-3 ml-3" type="reset">
+              Reset
+            </button>
+            {list.map(item => (
+              <div>{item}</div>
+            ))}
+          </div>
+        </Form>
+      )}
+    </Formik>
+  )
 }
 
-export default singup
+export default SignUp
