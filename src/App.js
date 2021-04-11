5a91399f681c2881bc1c5b5f0a35096a390eecae
Mohammad Habibi <mdhi2000@gmail.com>
2021-04-12 01:18:24 +0430

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import './App.scss';
-import Imgadd from './ax.png';
-import Sign from './singup';
-import List from './List';
-import { BrowserRouter as Router, Switch, Route , Link } from "react-router-dom";
+import "./App.scss"
+import Imgadd from "./ax.png"
+import Sign from "./singup"
+import List from "./list"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 function App() {
   return (
     <Router>
-    <div className='container mt-3'>
-      <div className='row'>
-        <div className='col-sm-5'>
-        <Sign />
-        </div>
-        <div className='col-sm-7'>
-          <img  className='img-fluid w-100' src={Imgadd} className="img-fluid w-100"/>
-          <br/>
-          <br/>
-          <button><Link to="/List">Show List</Link></button>
-          <Switch>
-          <Route path="/List" component={List}>
-          </Route>
-         </Switch>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-sm-7">
+            <img className="img-fluid w-100" src={Imgadd} className="img-fluid w-100" />
+          </div>
+          <div className="col-sm-5">
+            <Switch>
+              <Route exact path="/" component={Sign} />
+              <Route path="/List/:name/:family" component={List} />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/list.js
+++ src/list.js
@@ -1,14 +1,24 @@
-import React from 'react'
-let name = localStorage.getItem('MyList');
-function list(props) {
-    return (
-        <div> 
-        <h1>The List is :</h1>
-        <br/>
-        <br/>
-        {name}    
-        </div>
-    )
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router"
+// let name = localStorage.getItem('MyList');
+function List(props) {
+//   const [name, setName] = useState()
+  const {name, family} = useParams()
+
+//   useEffect(() => {
+//     setName(localStorage.getItem("MyList"))
+//   }, [])
+
+  return (
+    <div>
+      <h1>The List is :</h1>
+      <br />
+      <br />
+      {name}
+      <br />
+      {family}
+    </div>
+  )
 }
 
-export default list
+export default List
