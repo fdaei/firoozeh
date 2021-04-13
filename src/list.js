2ab1d82e6b3ca8b84cd78a64fe1e8eb8c83e4a48
fdaei <nasimdaei8@gmail.com>
2021-04-13 21:37:12 +0430

    test

--- src/list.js
+++ src/list.js
@@ -1,13 +1,19 @@
+import App from './App';
+import { BrowserRouter as Router, Switch, Route , Link } from "react-router-dom";
 import React from 'react'
 let name = localStorage.getItem('MyList');
 function list(props) {
     return (
+        <Router>
         <div> 
         <h1>The List is :</h1>
         <br/>
+        {name} 
         <br/>
-        {name}    
+        <button>exit</button>  
+        <button>exit and clear</button>
         </div>
+        </Router>
     )
 }
 
